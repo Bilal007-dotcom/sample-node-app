51a071c084f676293813872ddd01ae8646e20ecc
Shaikh Bilal <shaikhbilal9921@gmail.com>
2025-11-05 20:11:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('This is the website from github push task'));
+app.get('/', (req, res) => res.send('This is the website from github push task1'));
 app.listen(3000, () => console.log('App running on port 3000'));
