83fb0a7ddfb900e32c68bf9534787350cab4a724
Shaikh Bilal <shaikhbilal9921@gmail.com>
2025-11-05 19:51:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD with Docker & Jenkins!'));
+app.get('/', (req, res) => res.send('Now I have done from github'));
 app.listen(3000, () => console.log('App running on port 3000'));
